90c67ceb731de1043ed50bf75ae5ca59509fc607
Sergey <sergey.potapov.2002@mail.ru>
2022-08-31 08:58:10 +0500

    sortClassNames

---
+++ sortClassNames/sortClassNames.js
@@ -0,0 +1,21 @@
+const classNames = ['header', 'menu', 'menu-item', 'menu-item', 'menu-item', 'footer', 'menu', 'link', 'link', 'link', 'link'];
+
+const classNamesCount = {};
+const uniqClassNames = [];
+
+for (const i in classNames) {
+    const existing = classNamesCount[classNames[i]];
+    if (existing) {
+        classNamesCount[classNames[i]] += 1;
+    } else {
+        classNamesCount[classNames[i]] = 1;
+        uniqClassNames.push(classNames[i]);
+    };
+};
+
+const result = uniqClassNames.sort((a, b) => classNamesCount[b] - classNamesCount[a]);
+
+//  or without declaring uniqClassNames:
+//  const result = Object.keys(uniqClassNames).sort((a, b) => classNamesCount[b] - classNamesCount[a]);
+
+console.log(result);
